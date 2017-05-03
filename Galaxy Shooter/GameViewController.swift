3fa596703300a5895ac7b94ba296677a15a72b0c
Martin Kowaleff <martin.kowaleff@lc.cuny.edu>
2017-05-03 17:37:07 -0400

    Update GameViewController.swift

--- Galaxy Shooter/GameViewController.swift
+++ Galaxy Shooter/GameViewController.swift
@@ -40,6 +40,7 @@ class GameViewController: UIViewController {
             // set to true if you want to see nodes & frames
             skView.showsFPS = false
             skView.showsNodeCount = false
+            skView.showsPhysics = false
         
             /* Sprite Kit applies additional optimizations to improve rendering performance*/
             skView.ignoresSiblingOrder = true
